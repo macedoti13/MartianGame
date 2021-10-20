2941732be4f6573236dcb859024217ff3abbc4b3
Thiago Macedo <thialmacedo@gmail.com>
2021-10-20 18:08:19 -0300

    Improvements

--- Jogo.java
+++ Jogo.java
@@ -1,32 +1,48 @@
 public class Jogo {
+
     CircularDoubleLinkedList c = new CircularDoubleLinkedList();
-    public int jogada(int n) {
+
+    public int jogada(int n, int firstn) {
         if (n==1) {
             return c.count;
         }
         else {
             if (n%2 == 0) {
+                if (firstn <= 20) {
+                    c.printCirculo();
+                }
                 c.addBehind(n/2);
                 c.addInFront(n/2);
                 c.changePointerNodeValue(n/2);
                 c.movePointer(n);
-                jogada(c.valorPointer());
+                if (firstn <= 20) {
+                    c.printCirculo();
+                }
+                jogada(c.valorPointer(), firstn);
                 return c.count;
             }
             else {
+                if (firstn <= 20) {
+                    c.printCirculo();
+                }
                 c.addBehind(n-1);
                 c.addInFront(n-1);
                 c.changePointerNodeValue(n-1);
                 c.movePointer(n);
-                jogada(c.valorPointer());
+                if (firstn <= 20) {
+                    c.printCirculo();
+                }
+                jogada(c.valorPointer(), firstn);
                 return c.count;
             }
         }
     }
 
     public void jogo(int n) {
+        int firstn = n;
+        c.count = 0;
         c.addFirstElement(n);
-        System.out.println(jogada(n));
+        System.out.println(jogada(n, firstn));
     }
     
 }
--- app.java
+++ app.java
@@ -1,37 +1,7 @@
 public class app {
     public static void main(String[] args) {
-        //CircularDoubleLinkedList c = new CircularDoubleLinkedList();
-        /* 
-        System.out.println("começando o jogo");
-        c.addFirstElement(8);
-        //c.printCirculo();
-        c.addBehind(8/2);
-        c.addInFront(8/2);
-        c.changePointerNodeValue(8/2);
-//c.printCirculo();
-        c.movePointer(5, "right");
-        //c.printCirculo();
-        c.addBehind(4/2);
-        c.addInFront(4/2);
-        c.changePointerNodeValue(4/2);
-        //c.printCirculo();
-        c.movePointer(5, "right");
-        //c.printCirculo();
-        c.addBehind(2/2);
-        c.addInFront(2/2);
-        c.changePointerNodeValue(2/2);
-        //c.printCirculo();
-        c.movePointer(5, "right");
-        //c.printCirculo();
-        c.addBehind(2/2);
-        c.addInFront(2/2);
-        c.changePointerNodeValue(2/2);
-        c.movePointer(4, "right");
-        //c.printCirculo();*/
-        
-        
         Jogo j = new Jogo();
-        j.jogo(1);
-        
+        //System.out.println("Jogo com 21 elementos: "); 
+        j.jogo(21);
     }
 }
