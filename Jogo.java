32169de26626b8aa1d23ae4d2f192a6756958a4c
Thiago Macedo <thialmacedo@gmail.com>
2021-10-20 21:35:16 -0300

    comments added

--- Jogo.java
+++ Jogo.java
@@ -2,6 +2,14 @@ public class Jogo {
 
     CircularDoubleLinkedList c = new CircularDoubleLinkedList();
 
+    /**
+     * Metodo que chama uma jogada do jogo marciano. Recebe um parametro n, que se for impar, adciona ele mesmo subtraindo 1 na frente e atrás, 
+     * muda seu proprio valor para ficar igual aos vizinhos, move o pointer para a esquerda ou direita, dependendo do valor original do primeiro
+     * pointer, e por fim, chama a si mesmo com o valor no novo pointer.
+     * @param n Valor do pointer da jogada
+     * @param firstn Valor do pointer original. Pointer que iniciou o jogo.
+     * @return o numero de elementos no circulo ao final da jogada
+     */
     public int jogada(int n, int firstn) {
         if (n <= 1) {
             System.out.print("Elementos no circulo no fim do jogo: ");
@@ -39,6 +47,10 @@ public class Jogo {
         }
     }
 
+    /**
+     * Chama o jogo, adcionando o primeiro elemento, salvando o valor original do primeiro pointer e chamando o metodo recursivo da jogada.
+     * @param n Valor do primeiro elemento/ valor original do pointer.
+     */
     public void jogo(int n) {
         int firstn = n;
         c.count = 0;
